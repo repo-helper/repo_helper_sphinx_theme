d3797851a703e5b48f55d1d7d96c68410257e1b9
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-07-26 21:33:52 +0100

    Becoming repo_helper_sphinx_theme

---
+++ repo_helper_sphinx_theme/__init__.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+#
+#  __init__.py
+"""
+Sphinx Theme for repo_helper.
+"""
+#
+#  Copyright (c) 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  Based on Alabaster: https://github.com/bitprophet/alabaster/
+#  Copyright (c) 2020 Jeff Forcier.
+#
+#  Based on original work copyright (c) 2011 Kenneth Reitz and copyright (c) 2010
+#  Armin Ronacher.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without modification,
+#  are permitted provided that the following conditions are met:
+#
+#      * Redistributions of source code must retain the above copyright notice,
+#        this list of conditions and the following disclaimer.
+#      * Redistributions in binary form must reproduce the above copyright notice,
+#        this list of conditions and the following disclaimer in the documentation
+#        and/or other materials provided with the distribution.
+#      * Neither the name of the copyright holder nor the names of its contributors may
+#        be used to endorse or promote products derived from this software
+#        without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+#  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# stdlib
+import os
+
+__author__: str = "Dominic Davis-Foster"
+__copyright__: str = "2020 Dominic Davis-Foster"
+
+__license__: str = "BSD"
+__version__: str = "0.0.0"
+__email__: str = "dominic@davis-foster.co.uk"
+
+
+def get_path():
+	"""
+    Shortcut for users whose theme is next to their conf.py.
+    """
+	# Theme directory is defined as our parent directory
+	return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+def update_context(app, pagename, templatename, context, doctree):
+	context["repo_helper_sphinx_theme_version"] = __version__
+
+
+def setup(app):
+	# add_html_theme is new in Sphinx 1.6+
+	if hasattr(app, "add_html_theme"):
+		theme_path = os.path.abspath(os.path.dirname(__file__))
+		app.add_html_theme("repo_helper_sphinx_theme", theme_path)
+	app.connect("html-page-context", update_context)
+	return {"version": __version__, "parallel_read_safe": True}
