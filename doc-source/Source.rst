3db76d68de54a59db81d1204fb86e598cda440be
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-20 18:42:53 +0000

    Switch CI to GitHub Actions.

--- doc-source/Source.rst
+++ doc-source/Source.rst
@@ -3,7 +3,7 @@ Downloading source code
 =========================
 
 The ``repo_helper Theme`` source code is available on GitHub,
-and can be accessed from the following URL: https://github.com/domdfcoding/repo_helper_sphinx_theme"
+and can be accessed from the following URL: https://github.com/domdfcoding/repo_helper_sphinx_theme
 
 If you have ``git`` installed, you can clone the repository with the following command:
 
--- doc-source/conf.py
+++ doc-source/conf.py
@@ -8,7 +8,7 @@ import re
 import sys
 
 sys.path.append(os.path.abspath('.'))
-sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath(".."))
 
 # this package
 from __pkginfo__ import __version__
@@ -27,67 +27,66 @@ project = "repo_helper Theme"
 slug = re.sub(r'\W+', '-', project.lower())
 release = version = __version__
 copyright = "2020 Dominic Davis-Foster"  # pylint: disable=redefined-builtin
-language = 'en'
+language = "en"
 package_root = "repo_helper_sphinx_theme"
 
 extensions = [
-		'sphinx_toolbox',
-		'sphinx_toolbox.more_autodoc',
-		'sphinx_toolbox.more_autosummary',
-		'sphinx_toolbox.tweaks.param_dash',
-		'sphinx.ext.intersphinx',
-		'sphinx.ext.mathjax',
-		'sphinxcontrib.httpdomain',
-		'sphinxcontrib.extras_require',
-		'sphinx.ext.todo',
-		'sphinxemoji.sphinxemoji',
-		'notfound.extension',
-		'sphinx_copybutton',
-		'sphinxcontrib.default_values',
-		'sphinxcontrib.toctree_plus',
-		'seed_intersphinx_mapping',
+		"sphinx_toolbox",
+		"sphinx_toolbox.more_autodoc",
+		"sphinx_toolbox.more_autosummary",
+		"sphinx_toolbox.tweaks.param_dash",
+		"sphinx.ext.intersphinx",
+		"sphinx.ext.mathjax",
+		"sphinxcontrib.httpdomain",
+		"sphinxcontrib.extras_require",
+		"sphinx.ext.todo",
+		"sphinxemoji.sphinxemoji",
+		"notfound.extension",
+		"sphinx_copybutton",
+		"sphinxcontrib.default_values",
+		"sphinxcontrib.toctree_plus",
+		"seed_intersphinx_mapping",
 		]
 
-sphinxemoji_style = 'twemoji'
+sphinxemoji_style = "twemoji"
 todo_include_todos = bool(os.environ.get("SHOW_TODOS", 0))
 gitstamp_fmt = "%d %b %Y"
 
-templates_path = ['_templates']
-html_static_path = ['_static']
-source_suffix = '.rst'
+templates_path = ["_templates"]
+html_static_path = ["_static"]
+source_suffix = ".rst"
 exclude_patterns = []
 
-master_doc = 'index'
-suppress_warnings = ['image.nonlocal_uri']
-pygments_style = 'default'
+master_doc = "index"
+suppress_warnings = ["image.nonlocal_uri"]
+pygments_style = "default"
 
 intersphinx_mapping = {
-		'python': ('https://docs.python.org/3/', None),
-		'sphinx': ('https://www.sphinx-doc.org/en/stable/', None),
+		"python": ("https://docs.python.org/3/", None),
+		"sphinx": ("https://www.sphinx-doc.org/en/stable/", None),
 		}
 
-html_theme = 'repo_helper_sphinx_theme'
+html_theme = "repo_helper_sphinx_theme"
 html_theme_options = {
-		'page_width': '1200px',
-		'logo_name': 'true',
-		'github_user': 'domdfcoding',
-		'github_repo': 'repo_helper_sphinx_theme',
-		'description': 'Sphinx Theme for repo_helper.',
-		'github_banner': 'true',
-		'github_type': 'star',
-		'badge_branch': 'master',
-		'fixed_sidebar': 'true',
+		"page_width": "1200px",
+		"logo_name": "true",
+		"github_user": "domdfcoding",
+		"github_repo": "repo_helper_sphinx_theme",
+		"description": "Sphinx Theme for repo_helper.",
+		"github_banner": "true",
+		"github_type": "star",
+		"badge_branch": "master",
+		"fixed_sidebar": "true",
 		}
 html_theme_path = ["../.."]
 html_show_sourcelink = True  # True will show link to source
 
 html_context = {}
-
 htmlhelp_basename = slug
 
-latex_documents = [('index', f'{slug}.tex', project, author, 'manual')]
-man_pages = [('index', slug, project, [author], 1)]
-texinfo_documents = [('index', slug, project, author, slug, project, 'Miscellaneous')]
+latex_documents = [("index", f'{slug}.tex', project, author, "manual")]
+man_pages = [("index", slug, project, [author], 1)]
+texinfo_documents = [("index", slug, project, author, slug, project, "Miscellaneous")]
 
 toctree_plus_types = {
 		"class",
@@ -131,9 +130,9 @@ autodoc_exclude_members = [   # Exclude "standard" methods.
 		"__hash__",
 		]
 autodoc_default_options = {
-		'members': None,  # Include all members (methods).
-		'special-members': None,
+		"members": None,  # Include all members (methods).
+		"special-members": None,
 		"autosummary": None,
 		"show-inheritance": None,
-		'exclude-members': ','.join(autodoc_exclude_members),
+		"exclude-members": ','.join(autodoc_exclude_members),
 		}
